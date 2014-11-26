5b88cd6558ed19881d06abf2239eb7efc21c2ddd
wadehuber <wadehuber@gmail.com>
2014-11-26 16:30:38 -0700

    Initial version

---
+++ c/stringexample.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+main() {
+	int ii;
+	char s[5];
+	char s1[ ] = "Hello";
+	char s2[5] = "World";
+
+	/* Print character by character */
+	printf("s1: ");
+	for (ii = 0; ii < 5; ii++) 
+		printf("%c", s1[ ii ]); 
+	printf("\ns2: ");
+	for (ii = 0; ii < 5; ii++) 
+		printf("%c", s2[ ii ]); 
+
+	/* Print using printf 
+	 *  
+	 */
+	printf("\n\ns1:[%s]\ns2:[%s]\n",s1,s2);
+
+	printf ("\nSizes: s1=%d, s2=%d\n", sizeof(s1), sizeof(s2));
+
+	printf("End of string character (s1): %d\n", s1[sizeof(s1)-1]);
+}
