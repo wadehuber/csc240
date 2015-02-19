9f373149cbe9a993de1a2b4e5a38feacd78c5ad3
wadehuber <wadehuber@gmail.com>
2015-02-19 16:06:16 -0700

    Initial versions

---
+++ c/functions.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/* forward declarations / prototypes */
+int increment1 (int n);
+void increment2 (int * n);
+
+int main() {
+    int x = 4, y=0;
+    printf("x = %d,  y = %d\n", x, y);
+    y=increment1(x);
+    printf("x = %d,  y = %d\n", x, y);
+    increment2(&x);  /* pass the ADDRESS since formal parameter is a POINTER */
+    printf("x = %d,  y = %d\n", x, y);
+    printf("x = %d, increment1(x)=%d\n", x, increment1(x));
+    printf("x = %d,  y = %d\n", x, y);
+}
+
+
+/* function returns a value.  Call-by-value - actual parameter gets copied
+ *   into local variable n.
+ *   The actual parameter is not modified */
+
+int increment1 (int n) {
+    return n+1;
+}
+
+/* procedure does not return a value.  Call-by-address - actual parameter 
+ *   gets copied into local variable n, but since n is a pointer it
+ *   functions as an alias of the actual parameter
+ *   The actual parameter IS modified (and should be an address!) */
+
+void increment2 (int * n) {
+    (*n)++;
+}
---
+++ c/recursion.c
@@ -0,0 +1,52 @@
+/* examples of recursion in c */
+#include<stdio.h>
+
+int fact1(int n);
+int fact2(int n);
+
+
+int main () {
+   printf("fact1: 10!=%d\n", fact1(10));
+   printf("fact2: 10!=%d\n", fact2(10));
+   printf("fact3: 10!=%d\n", fact3(10));
+}
+
+/* compute the factorial of n using recursion */
+int fact1(int n) {
+    if (n<=1) {
+        return 1;
+    }
+    else {
+        /* note that when the factorial returns we still need to 
+         *   multiply result by 10 */
+        return n* fact1(n-1);
+    }
+}
+
+int fact2(int n) { 
+    return fact2tail(n, 1);
+}
+/* compute factorial - tail recursion.  Note that we don't have to
+ *   do anything with the return value, we only return it */
+int fact2tail(int n, int res) {
+    if (n<=1) {
+        return res;
+    }
+    else {
+        return fact2tail(n-1, n * res);
+    }
+}
+
+/* iterative method with similar computational structure to fact2 
+ *   note that n is used as a local variable */
+int fact3(int n) { 
+    int res = 1;
+    while (n > 0) {
+        res = res * n;
+        n--;
+    }
+    return res;
+}
+
+
+
