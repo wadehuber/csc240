f06d5c71c20dd6daa9537606d9e30b9f4290286a
wadehuber <wadehuber@gmail.com>
2015-07-12 15:47:38 -0700

    Added list reverse example.

--- scheme/tailrecursion.scm
+++ scheme/tailrecursion.scm
@@ -1,6 +1,7 @@
 #lang scheme
 
-; find factorial using recursive definition
+; Example 1: Factorial numbers
+; find factorial using the straightforward recursive definition
 (define (factorial n)
   (if (<= n 0)
       1
@@ -15,38 +16,72 @@
       acc
       (factorial-tail-acc (- n 1) (* n acc))))
 
-;; Test cases
-(display "factorial - values 1, 4, 10, 100")(newline)
-(factorial 1)
-(factorial 4)
-(factorial 10)
-(factorial 100)
 
-(newline)
-(display "factorial-tail - values 1, 4, 10, 100")(newline)
-(factorial-tail 1)
-(factorial-tail 4)
-(factorial-tail 10)
-(factorial-tail 100)
-
-
-; Tail-recursive Fibonacci implementation
+; Example 2: Fibonacci numbers
+; find fibonacci(n) using the straightforward recursive definition
 (define fib
-  (lambda (x)
-    (if (<= x 0)
+  (lambda (n)
+    (cond 
+      ((<= n 0) 0)
+      ((= n 1) 1)
+      (else (+ (fib (- n 2)) (fib (- n 1)))))))
+ 
+; Tail-recursive Fibonacci implementation
+(define fib-tail
+  (lambda (n)
+    (if (<= n 0)
         0
-        (fib-helper x 0 1))))
+        (fib-tail-acc n 0 1))))
 
-(define fib-helper
+(define fib-tail-acc
   (lambda (index f2 f1)
     (if (<= index 0)
         f2
-        (fib-helper (- index 1) f1 (+ f1 f2)))))
+        (fib-tail-acc (- index 1) f1 (+ f1 f2)))))
 
-(newline)
+
+; Example 3: List reverse
+; Straitforward list reverse implementation 
+(define rev
+  (lambda (lst)
+    (if (or (not (list? lst)) (empty? lst))
+        '()
+        (append (rev (cdr lst)) (list (car lst))))))
+
+; Tail-recursive list reverse implementation
+(define rev-tail
+  (lambda (lst)
+    (rev-tail-acc lst '())))
+(define rev-tail-acc
+  (lambda (lst res)
+    (if (or (not (list? lst)) (empty? lst))
+        res
+        (rev-tail-acc (cdr lst) (cons (car lst) res)))))
+
+; Test function:  apply func to each member of lst
+(define test-list 
+  (lambda (func lst)
+    (if (or (not (list? lst)) (empty? lst))
+        (newline)  
+        (begin
+          (write (func (car lst))) (newline)
+          (test-list func (cdr lst))))))
+
+; Test cases
+(display "factorial - values 1, 4, 10, 100")(newline)
+(test-list factorial '(1 4 10)) 
+
+(display "factorial-tail - values 1, 4, 10, 100")(newline)
+(test-list factorial-tail '(1 4 10))
+
+(display "fib - values 1, 4, 10, 100")(newline)
+(test-list fib '(1 4 10))
+
 (display "fibonacci-tail - values 1, 4, 10, 100")(newline)
-(fib 1)
-(fib 4)
-(fib 10)
-(fib 100)
-(fib 1000)
+(test-list fib '(1 4 10))
+
+(display "rev - values '(a b c d) '(1 2 3) '())")(newline)
+(test-list rev '((a b c d) (1 2 3) ()))
+
+(display "rev-tail - values '(a b c d) '(1 2 3) '())")(newline)
+(test-list rev-tail '((a b c d) (1 2 3) ()))
