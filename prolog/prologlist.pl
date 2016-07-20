3edb0c9a99d2f213071597fc14e35646ffa59bf8
Wade Huber <wadehuber@gmail.com>
2016-07-19 21:49:28 -0700

    Update prologlist.pl

--- prolog/prologlist.pl
+++ prolog/prologlist.pl
@@ -1,16 +1,20 @@
 % Prolog list rule examples
 
 % Change everyting in a list to 'secret'
+% Rule 1: Base case
+% Rule 2: If there is a head, ignore it and cons 'secret' and the changed tail
 change([], []) :- !.   % base case
 change([_H| T], [secret | T1]) :- change(T, T1).  
-								 
-% test change
-:- change([1, 2, 3], X), write(X), nl.
 
 
 % Convert all negative numbers in a list to positive
+% Rule 1: Base case
+% Rule 2: Multiply by -1 if H is negative
+% Rule 3: H is positive, so leave it alone
 positive([], []) :- !.
 positive([H| T], [HP | T1]) :- H < 0, HP is H * -1, positive(T, T1).
 positive([H| T], [H | T1]) :- positive(T, T1).
 								 
+% tests
+:- change([1, 2, 3], X), write(X), nl.								 
 :- positive([-1, 2, -3], X), write(X), nl.
