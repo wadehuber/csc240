ca2fef567a02dffa6036b01e764498ba5d3e5b31
Wade Huber <wadehuber@gmail.com>
2016-09-09 20:56:37 -0700

    Added method & const int example

--- c/constparm.c
+++ c/constparm.c
@@ -4,7 +4,20 @@
 /* Preprocessor constant - no memory allocated */
 #define NUM 5
 
+/* const indicates that the parameter will not be changed, NOT that 
+ *  only a constant value is acceptable */
 int sum_num(const int a, const int b);
+
+/* const pointers prevent the variable being pointed to from being
+ * changed */
+int add_one(const int * a) {
+   int result;
+   /* Not allowed:
+     (*a)++; 
+   */
+   result = *a + 1; 
+   return (result);
+}
 
 int main()
 {
@@ -12,6 +25,10 @@ int main()
     int y = 2;
     printf("sum_num(x,y) = %d\n", sum_num(x, y));
     printf("sum_num(NUM,10) = %d\n", sum_num(NUM, 10));
+    printf("add_one(&x) = %d\n", add_one(&x));
+    /* Not allowed: the parameter has to be an address 
+    printf("add_one(NUM) = %d\n", add_one(NUM));
+    */
     return 0;
 }
 
