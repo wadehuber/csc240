04a8b822d9d1ef7124b94ab5476a33bb8abb1551
Wade Huber <wadehuber@gmail.com>
2016-09-13 16:59:00 -0700

    Cleared warnings & added comments

--- c/constants.c
+++ c/constants.c
@@ -1,9 +1,10 @@
-/* Example of constants in C */
+/* Example of three types of constants in C */
 #include <stdio.h>
 
 /* Preprocessor constant - no memory allocated */
 #define NUM 5
 
+/* Enumeration constants */
 typedef enum {TEN=10, TWENTY=20, THIRTY=30} values_t;
 typedef enum {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY} classdays_t;
 
@@ -24,13 +25,13 @@ int main()
     */
 	j = &const_var;	
 	printf("const_var = %d\n", const_var);	 /* const_var = 5 */
-	printf("j = %p (%d)\n", j, *j);	 /* const_var = 5 */
+	printf("j = %p (%d)\n", (void *) j, *j);	 /* const_var = 5 */
 
     /* You can change a constant variable through a pointer, so be
      *   be careful!  (This doesn't work with a preprocessor constant */
 	*j = *j + 2;	 
 	printf("const_var = %d\n", const_var);   /* const_var = 7? */
-	printf("j = %p (%d)\n", j, *j);	 /* const_var = 7? */
+	printf("j = %p (%d)\n", (void *) j, *j);	 /* const_var = 7? */
 
 	return 0;
 }
