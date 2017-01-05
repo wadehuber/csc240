5dcd161da4775afe74931be4b5a9565c058e2084
Wade Huber <wadehuber@gmail.com>
2017-01-04 21:30:22 -0700

    CLeaned up tail recursive function, added iterative eample.

--- c/tailrecursion.c
+++ c/tailrecursion.c
@@ -7,12 +7,11 @@
 #endif
 
 int fib1counter[NUM];
-int fib2counter[NUM];
+int fibtailcounter[NUM];
 
 int fib1(int n);
-int fib2(int n);
-int fib2tail(int, int);
-int fib2tail_helper(int n, int f2, int f1);
+int fibtail(int n);
+int fibtail_helper(int n, int f2, int f1);
 
 
 int main () {
@@ -21,14 +20,14 @@ int main () {
 
     for (ii=0;ii<NUM;ii++) {
       fib1counter[ii]=0;
-      fib2counter[ii]=0;
+      fibtailcounter[ii]=0;
     }
 
     num=NUM-1;
-    printf("fib1(%d)=%d,   fib2(%d)=%d\n", num, fib1(num), num, fib2(num));
+    printf("fib1(%d)=%d,   fibtail(%d)=%d\n", num, fib1(num), num, fibtail(num));
     for (ii=0;ii<NUM;ii++) {
-      printf("fib1counter[%d] = %d,   fib2counter[%d] = %d\n", 
-              ii, fib1counter[ii], ii, fib2counter[ii]);
+      printf("fib1counter[%d] = %d,   fibtailcounter[%d] = %d\n", 
+              ii, fib1counter[ii], ii, fibtailcounter[ii]);
     }
    return 0;
 }
@@ -44,24 +43,29 @@ int fib1(int n) {
         return fib1(n-1) + fib1(n-2);
 }
 
-int fib2(int n) { 
-    return fib2tail(n, 1);
-}
 /* compute factorial - tail recursion.  Note that we don't have to
  *   do anything with the return value, we only return it */
-int fib2tail(int n, int res) {
-    if (n<=0) 
-        return 0;
-    else 
-        return fib2tail_helper(n, 0, 1);
+int fibtail(int n) {
+   return fibtail_helper(n, 0, 1);
 }
 
-int fib2tail_helper(int n, int f2, int f1) {
-    fib2counter[n]++;
-    if (n <= 1)
-        return f1;
-    else
-        return (fib2tail_helper(n-1, f1, f1+f2));
-
+int fibtail_helper(int n, int f2, int f1) {
+   fibtailcounter[n]++;
+   if (n <=1 )
+     return f1;
+   else
+     return fibtail_helper(n-1, f1, f1+f2);
 }
 
+/* iterative solution */
+int fibwhile(int n) {
+  int f=1, f1 = 1, f2 = 1;
+  int ii=n;
+  while (ii > 2) {
+    f = f1 + f2;
+    f2 = f1;
+    f1 = f; 
+    ii--; 
+  }
+  return f;
+}
